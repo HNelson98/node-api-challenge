c19411c1d47cae79a9fbcab651e7877f6c1404f6
Henry Nelson <Henry.Nelson89@gmail.com>
2020-05-08 08:37:05 -0700

    posting new projects works!

--- projectsRouter.js
+++ projectsRouter.js
@@ -13,5 +13,41 @@ router.get('/', (req, res) => {
       })
 })
 
+// GET project by id
+router.get('/:id', (req, res) => {
+    const id = Number(req.params.id)
+
+    Projects.get(id)
+    .then(project => {
+        if(project){
+            res.status(200).json(project)
+        } else {
+            res.status(404).json({error: "There is no project with that ID"})
+        }
+        
+      })
+      .catch(err => {
+        console.log('GET project err', err)
+        res.status(500).json({ errorMessage: 'could not find the project' })
+      })
+})
+
+//POST new project
+router.post('/', (req, res) =>{
+    const {name, description} = req.body
+
+    if (!name || !description){
+        res.status(400).json({errorMessage: 'missing the name or description'})
+    } else {
+        Projects.insert(req.body)
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err =>{
+            console.log('POST project err', err)
+        res.status(500).json({ errorMessage: 'there was an error in uploading the project to the database'})
+        })
+    }
+})
 
 module.exports = router;
